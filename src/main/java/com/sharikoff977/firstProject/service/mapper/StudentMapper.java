6eb8ad3622d23135306cdc407548e78be3df9195
frickua <frick.ua@gmail.com>
2022-04-19 19:30:16 +0300

    StudentMapper resolve entity reference fix.

--- src/main/java/com/sharikoff977/firstProject/service/mapper/StudentMapper.java
+++ src/main/java/com/sharikoff977/firstProject/service/mapper/StudentMapper.java
@@ -2,33 +2,33 @@ package com.sharikoff977.firstProject.service.mapper;
 
 import com.sharikoff977.firstProject.facades.dto.StudentDTO;
 import com.sharikoff977.firstProject.model.Student;
+import com.sharikoff977.firstProject.repo.StudentRepo;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
+import org.mapstruct.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *Mapper for the entity {@link com.sharikoff977.firstProject.model.Student}
  * and its DTO {@link com.sharikoff977.firstProject.facades.dto.StudentDTO}
  * */
 @Mapper(uses = {SchoolClassMapper.class}, componentModel = "spring")
-public interface StudentMapper extends EntityMapper<StudentDTO, Student>{
+public abstract class StudentMapper implements EntityMapper<StudentDTO, Student> {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
     @Mapping(source = "schoolClassId", target = "schoolClassId")
-    StudentDTO toDto(Student student);
+    public abstract StudentDTO toDto(Student student);
 
     @Mapping(source = "schoolClassId", target = "schoolClassId"/*, qualifiedByName = "someMethod"*/)
-    Student toEntity(StudentDTO studentDTO);
+    public abstract Student toEntity(StudentDTO studentDTO);
 
-    /*@Named("someMethod")
-    default Object ghfhfjhfjj(Long id){
-        return new Object();
-    }*/
-
-    default Student fromId(Long id){
-        if (id == null){
-            return null;
-        }
-        Student student = new Student();
-        student.setId(id);
-        return student;
+    public Student fromId(Long id) {
+        return studentRepo.getById(id);
     }
 }
